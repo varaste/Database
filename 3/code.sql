7ca9683403ebda28405f84b84522bbf7cff6561b
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-11-23 00:27:55 +0330

    3

--- 3/code.sql
+++ 3/code.sql
@@ -1,4 +1,3 @@
 SELECT title;
-FROM Movie , Rating
-WHERE stars not 0 and stars not 1 and stars not 2 and stars not 3 and stars not 4
-and stars not 5;
+FROM Movie natural join Rating
+WHERE stars is null
