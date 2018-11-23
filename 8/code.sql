eaa6e0ea84bd840c47366d32cab48d3940ce0bb5
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-11-23 10:15:20 +0330

    8

--- 8/code.sql
+++ 8/code.sql
@@ -1,3 +1,3 @@
-select title , max(stars)-min(stars) as rating spread
-from Movie , Rating 
-where Rating.mID = Movie.mID group by title order by rating spread desc;
+select title, max(stars) - min(stars) as RatingSpread 
+from Movie , Rating  
+where Movie.mID = Rating.mID group by title order by RatingSpread desc, title;
