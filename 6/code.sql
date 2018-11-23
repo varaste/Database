a6eca6f771a4d04855723723144743b01b82d38d
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-11-23 09:53:26 +0330

    6

--- 6/code.sql
+++ 6/code.sql
@@ -1,5 +1,5 @@
-select Re1.name, M1.title
-from Movie M1, Reviewer Re1, Rating Ra1, Rating Ra2 
-where M1.mID = Ra1.mID and Re1.rID = Ra1.rID and
- Ra1.rID = Ra2.rID and Ra1.mID = Ra2.mID and
-  Ra1.ratingDate < Ra2.ratingDate and Ra1.stars < Ra2.stars;
+select name, title0
+from Reviewer, Movie, Rating, Rating Rating2
+where Rating.mID = Movie.mID and Reviewer.RatingID = Rating.ratingID 
+  and Rating.RatingID = Rating2.ratingID and Rating2.mID = Movie.mID
+  and Rating.stars < Rating2.stars and Rating.ratingDate < Rating2.ratingDate;
