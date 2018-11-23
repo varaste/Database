3fb033b9537f79566da951816fcb26e808ad94a3
Arya Varaste <30382009+varaste@users.noreply.github.com>
2018-11-23 09:59:22 +0330

    7

--- 7/code.sql
+++ 7/code.sql
@@ -1,4 +1,4 @@
-select M1.title, max(Ra1.stars) 
-from Movie M1, Rating Ra1
-where M1.mID = Ra1.mID
-    group by M1.title order by M1.title;
+select title, max(stars) 
+from Movie , Rating
+where Movie.mID = Rating.mID
+    group by title order by title;
