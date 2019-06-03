e88338dfd0f267420c61843d2e238fe8f7853197
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-06-03 12:35:25 +0430

    Delete code.sql

--- 6/code.sql
+++
@@ -1,5 +0,0 @@
-select name, title0
-from Reviewer, Movie, Rating, Rating Rating2
-where Rating.mID = Movie.mID and Reviewer.RatingID = Rating.ratingID 
-  and Rating.RatingID = Rating2.ratingID and Rating2.mID = Movie.mID
-  and Rating.stars < Rating2.stars and Rating.ratingDate < Rating2.ratingDate;
