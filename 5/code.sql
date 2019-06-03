004aa1b2657f72fe3240ea48679303cbaffc5ba1
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-06-03 12:36:31 +0430

    Delete code.sql

--- 5/code.sql
+++
@@ -1,5 +0,0 @@
-
-select Reviewer.name, Movie.title, Rating.stars, Rating.ratingDate 
-from Movie , Reviewer , Rating
-where Reviewer.rID = Rating.rID and Movie.mID = Rating.mID 
-    order by Reviewer.name, Movie.title, Rating.stars;
