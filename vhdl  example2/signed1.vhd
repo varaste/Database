38134b70f1f4c8a927abd869b2b2aefcf667a771
Arya Varaste <30382009+varaste@users.noreply.github.com>
2019-09-15 23:24:01 +0430

    Update signed1.vhd

--- vhdl  example2/signed1.vhd
+++ vhdl  example2/signed1.vhd
@@ -2,11 +2,4 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_signed.all;	-- (1)
 
-entity signed1 is
-p
-
-architecture behavior of signed1 is
-begin
-	addo <= a + b;	--(2)
-	subo <= a - b;	--(3)
-end;
+entity 
